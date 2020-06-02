6d07766b6cbb5913bef5286409077cde252e06d6
gauravm-bit <gauravwrk01@gmail.com>
2020-06-02 13:18:25 +0530

    ADDED: UI for login of fundoo notes

---
+++ src/components/Login.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import './Login.css';
+
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <Card className="loginCard" vairant="outlined">
+          <div className='logintitle'>
+            <h3 className="loginHead">Fundoo</h3>
+            <div className='loginText'>
+              <h3>Login into your Fundoo account</h3>
+              <form>
+                <div>
+                  <TextField className='loginTextField'
+                    label='Email'
+                    name='email'
+                    margin='normal'
+                    variant='outlined'
+                  >
+                  </TextField>
+                </div>
+                <div>
+                  <TextField className='loginTextField'
+                    label='Password'
+                    name='password'
+                    type='password'
+                    margin='normal'
+                    autoComplete='off'
+                    variant='outlined'
+                  >
+                  </TextField>
+                </div>
+              </form>
+              <div>
+                <div className='forgot'>
+                  <a className='Link' href='/forgot'>Forgot password?</a>
+                </div>
+                <div className='loginButton'>
+                  <Button color='primary'
+                    margin='normal'
+                    variant='contained'
+                  > Submit</Button>
+                </div>
+              </div>
+               <div className='register'>
+                <h3>Not registered yet?</h3>
+                <a className='Link' href='/register'>Create Fundoo Account</a>
+              </div>
+              
+            </div>
+          </div>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
+
